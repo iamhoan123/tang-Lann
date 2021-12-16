682859120f29b70877bdb783baea6dd5f24391f7
iamhoan123 <96246884+iamhoan123@users.noreply.github.com>
2021-12-16 21:51:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 He luu ! 🖐🖐🖐',
+    introDesc: `Web này tạo ra chỉ để chúc Lan giáng sinh vui vẻ`,
+    btnIntro: 'OK',
+    title: 'Merry Christmas🎄🎄🎄🎄',
+    desc: 'Ráng mà bấm trúng nhaaaa 🥳🥳🥳🥳',
+    btnYes: 'Bấm vào đây thì phải tặng quà😇😇',
+    btnNo: 'Bấm vào đây là được nhận quà 👝👝👝',
+    question:'Có cố gắng đấy 👏👏 thôi lần này tha cho chọn quà',
+    reasonPlaceholder: 'Mún quà gì nào',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Hoàn tặng gì cũng thích hết á🥰🥰',
+    mess: 'Ui Hoàn biết mà đợi giáng sinh nhaa🥳🥳',
+    messDesc: '--Muốn chọn quà hay bí mật nào--',
+    btnAccept: 'Bí mật đi 🙆‍♀️🙆‍♀️ ',
+    messLink: 'https://www.facebook.com/messages/t/100011360058897'
+}
